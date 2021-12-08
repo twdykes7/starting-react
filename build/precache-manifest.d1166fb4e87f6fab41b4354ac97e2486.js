805b6d195762e6b57cd351c99d43cd451706c566
Timothy Dykes <timothywanedykes@gmail.com>
2021-12-07 18:49:45 -0800

    initial commit

---
+++ build/precache-manifest.d1166fb4e87f6fab41b4354ac97e2486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44e43f2fdcb97959d357d90472b18bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e6411499a8359b31d52",
+    "url": "/static/css/main.7057c4f0.chunk.css"
+  },
+  {
+    "revision": "dd9af166fea3ef6fbb9e",
+    "url": "/static/js/2.334f5c5f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.334f5c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6411499a8359b31d52",
+    "url": "/static/js/main.a3ecb9b4.chunk.js"
+  },
+  {
+    "revision": "5132f4db0caab9ba715b",
+    "url": "/static/js/runtime-main.30538d72.js"
+  }
+]);
